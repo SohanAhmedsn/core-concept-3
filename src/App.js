c2855096ba2470ecbc5e931c30c0c1340d5e8e8c
sn77sohan <sohan.ahmedself@gmail.com>
2021-11-07 20:33:40 +0600

    number-increase-decrease

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+
+
 function App() {
   return (
     <div className="App">
+      <h2>Module No: 45_7-Advance state, component state hook and set state Method</h2>
      <Counter></Counter>
     
     </div>
@@ -13,14 +16,14 @@ function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
-  const handleIncrease = () => {
-    const newCount = count + 1;
-    setCount(newCount);
-  }
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount(count - 1);
+  
   return (
     <div>
       <h1>Count: {count}</h1>
        <button onClick={handleIncrease}>Increase</button>
+       <button onClick={handleDecrease}>Decrease</button>
     </div>
   )
 }
@@ -40,3 +43,18 @@ function Counter() {
 //   )
 // }
 export default App;
+
+
+
+
+
+
+
+
+
+// const handleIncrease = () => {
+//   const newCount = count + 1;
+//   setCount(newCount);
+
+//   // const handleIncrease = () => setCount(count + 1)
+// }
